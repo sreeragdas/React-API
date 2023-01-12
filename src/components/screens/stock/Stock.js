74826c11e6294c178c8204f3728e90ff35e92e7a
EmbarckleSreerag <sreerag.em@embarckle.com>
2023-01-12 18:15:47 +0530

    chore(client):Search functionality added

--- src/components/screens/stock/Stock.js
+++ src/components/screens/stock/Stock.js
@@ -8,27 +8,29 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useNavigate } from "react-router";
 import axios from "axios";
+import Fuse from 'fuse.js';
 const  Stock = ()=> {
-
+const [query , setQuery] = React.useState("");
   const [csvArray, setCsvArray] = React.useState([]);
-
-  
+const [searchData , setSearchData] = React.useState([]);
+  const [filterVal ,setFilterVal] = React.useState('');
   const getData = () => {
   
     axios
       .get(`https://prototype.sbulltech.com/api/v2/instruments`)
       .then((res) => {
+        setSearchData(res.data)
         processCSV(res.data);
        
       });
-    
+
   };
   React.useEffect(() => {
   
     getData();
 
   }, []);
-
+console.log(searchData , 'seachdatatatata==-=-=-')
 
   const processCSV = (str, delim = ",") => {
     const headers = str.slice(0, str.indexOf("\n")).split(delim);
@@ -44,23 +46,45 @@ const  Stock = ()=> {
     });
 
     setCsvArray(newArray);
-  };
-  console.log(csvArray, "-----------");
-  console.log(typeof csvArray);
+    setSearchData(csvArray)
 
-  const rowsHead = Object.entries(csvArray);
+  };
+
+
+
   const rows = Object.values(csvArray);
  
-  console.log(rowsHead);
-  const navigate = useNavigate();
- 
-  return (
 
+  const navigate = useNavigate();
+
+console.log(searchData,'data')
+console.log( csvArray, 'csvsvsvsvsv')
+
+  const fuse = new Fuse(csvArray , {
+    keys:[
+      'Name',
+      'Symbol'
+    ]
+  })
+const result = fuse.search(query)
+console.log(result , 'result ++++++++++++++++++++++++++++++++++++++++++++++')
+const csvArrayResult = result.map(result =>result.item)
+
+const handleSearch = ({currentTarget={}}) =>{
+  const {value} = currentTarget;
+  setQuery(value)
+
+
+}
+  return (
+<div>
+  <input placeholder="Search.." type="text" value={query} onChange={handleSearch}  />
     <TableContainer component={Paper}>
     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
       <TableHead>
         <TableRow>
           <TableCell>symbol</TableCell>
+          
           <TableCell>name</TableCell>
           <TableCell >sector</TableCell>
           <TableCell>validtill</TableCell>
@@ -72,16 +96,48 @@ const  Stock = ()=> {
             <TableCell component="th" scope="row">
      
             </TableCell>
-            {rows.map((item, index) => {
+            
+        {
+          query ? csvArrayResult.map((item, index) => {
+         
+        
+            return (
+              <TableRow>
+              <TableCell align="right" onClick={()=>{ navigate("/quotes", { state: item.Symbol })}}>{item.Symbol}</TableCell> 
+                <TableCell align="right">{item.Name}</TableCell>
+              
+               <TableCell align="right">{item.Sector}</TableCell>
+               <TableCell align="right">{item.Validtill}</TableCell> 
+                  </TableRow>
+          );
+          }) : csvArray.map((item, index) => {
+         
+        
+            return (
+              <TableRow>
+              <TableCell align="right" onClick={()=>{ navigate("/quotes", { state: item.Symbol })}}>{item.Symbol}</TableCell> 
+                <TableCell align="right">{item.Name}</TableCell>
+              
+               <TableCell align="right">{item.Sector}</TableCell>
+               <TableCell align="right">{item.Validtill}</TableCell> 
+                  </TableRow>
+          );
+          })
+        }
+
+           {/* {csvArrayResult.map((item, index) => {
+         
+        
          return (
            <TableRow>
            <TableCell align="right" onClick={()=>{ navigate("/quotes", { state: item.Symbol })}}>{item.Symbol}</TableCell> 
              <TableCell align="right">{item.Name}</TableCell>
+           
             <TableCell align="right">{item.Sector}</TableCell>
             <TableCell align="right">{item.Validtill}</TableCell> 
                </TableRow>
        );
-       })}
+       })}  */}
        
 
     
@@ -89,6 +145,7 @@ const  Stock = ()=> {
       </TableBody>
     </Table>
   </TableContainer>
+  </div>
   );
 }
 export default Stock;
